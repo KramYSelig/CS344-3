fd60f5c70047518f7e110dd1ea6e54ec9656e4d8
Mark Giles <gilesm@eos-class.engr.oregonstate.edu>
2015-11-22 07:35:13 -0800

    saving for sanity

--- smallsh.c
+++ smallsh.c
@@ -11,25 +11,32 @@
  *
  * ***************************************************************************/
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+struct ChildProcess {
+	pid_t pid;
+	int exitStatus,
+		termSignal;
+};
+
 struct Shell {
 	int status[2],
 		exit,
 		argCount,
-		child,
-		parent,
-		numChildPIDs;
-	pid_t childPIDs[200],
-		  exitPID,
-		  spawnPID;
+		numChildren;
 	char cwd[1024],
 		 buffer[2049],
 	     *args[512];
+	struct ChildProcess children[200];
 };
+
+
 
 // initialize global shell settings and attributes
 void initShell(struct Shell *sh);
@@ -42,36 +49,99 @@ int main(int argc, char *argv[]) {
 	struct Shell *sh = malloc(sizeof(struct Shell));
 	initShell(sh);
 	runShell(sh);
-
 	return 0;
 }
 
 
 void shellRunSysProc(struct Shell *sh) {
-	int bg = -1;
-	sh->spawnPID = fork();
+	int bg = -1,
+		i = 0,
+		inIndex = -1,
+		outIndex = -1,
+		fdRedirect = -5,
+		inputFD,
+		outputFD;
+	char outFile[512],
+		 inFile[512];
+	pid_t spawnPID,
+		exitPID;
+	fflush(0);
+	spawnPID = fork();
 	if (strcmp(sh->args[sh->argCount - 1], "&") == 0) {
 		bg = 1;
 		sh->args[sh->argCount - 1] = '\0';
 	}
-	switch(sh->spawnPID) {
+	for (i = 0; i < sh->argCount - 1; i++) {
+		if (strcmp(sh->args[i], "<") == 0) {
+			strcpy(inFile, sh->args[i + 1]);
+			if (inIndex == -1) {
+				inIndex = i;
+			}
+		} else if (strcmp(sh->args[i], ">") == 0) {
+			strcpy(outFile, sh->args[i + 1]);
+			if (outIndex == -1) {
+				outIndex = i;
+			}
+		}
+	}
+	if (inIndex != -1 && outIndex != -1) {
+		if (inIndex < outIndex) {
+			sh->args[inIndex] = NULL;
+		} else {
+			sh->args[outIndex] = NULL;
+		}
+	} else if (inIndex != -1 && outIndex == -1) {
+		sh->args[inIndex] = NULL;
+	} else if (outIndex != -1 && inIndex == -1) {
+		sh->args[outIndex] = NULL;
+	}
+
+		switch(spawnPID) {
 		case -1:
 			perror("Fork failed!");
 			break;
 		case 0:
-			if (bg == 1) {
-				
+			if (inIndex != -1) {
+				// open input file read only
+				inputFD = open(inFile, O_RDONLY);
+				fdRedirect = dup2(inputFD, 0);
+				if (fdRedirect == -1) {
+					perror("dup2");
+					exit(2);
+				}
 			}
-			execvp(sh->args[0], sh->args);
-			exit(0);
+			if (outIndex != -1) {
+				// open output file write only
+				outputFD = open(outFile, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+				fdRedirect = dup2(outputFD, 1);
+				if (fdRedirect == -1) {
+					perror("dup2");
+					exit(2);
+				}
+			}
+			errno = 0;
+			sh->status[0] = execvp(sh->args[0], sh->args);
+			if (sh->status[0] == -1 && errno != 0) {
+			
+			}
+			printf("%s: Command not found.\n", sh->args[0]);
+			exit;
 			break;
 		default:
+			sh->children[sh->numChildren].pid = spawnPID;
+			sh->numChildren++;
+			
 			if (bg != 1) {
-				sh->exitPID = waitpid(sh->spawnPID, &sh->status[0], 0);
+				exitPID = waitpid(spawnPID, &sh->status[0], 0);
+			} else {
+				exitPID = waitpid(spawnPID, &sh->status[0], WNOHANG);
 			}
 			if (WIFEXITED(sh->status[0])) {
-				int myExit = WEXITSTATUS(sh->status[0]);
-				printf("exit: %i\n", myExit);
+				sh->children[sh->numChildren - 1].exitStatus = WEXITSTATUS(sh->status[0]);
+				sh->status[1] = 0;
+			} else if (WIFSIGNALED(sh->status[0])) {
+				sh->children[sh->numChildren - 1].termSignal = WTERMSIG(sh->children[sh->numChildren - 1].exitStatus);
+				sh->status[1] = 1;
 			}
 			break;
 	}
@@ -84,9 +154,7 @@ void initShell(struct Shell *sh) {
 	sh->exit = 0;						// do not exit loop until this changes
 	sh->argCount = 0;					// starting with 0 arguments
 	getcwd(sh->cwd, sizeof(sh->cwd));	// sets initial working directory
-	sh->numChildPIDs = 0;
-	sh->parent = -5;
-	sh->child = -5;
+	sh->numChildren = 0;
 }
 
 void shellProcessArgs(struct Shell *sh) {
